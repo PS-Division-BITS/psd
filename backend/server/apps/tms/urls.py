312293f5acd69b228e7d0b10002ac85bad6021fc
Nrupesh <nrupeshsurya@gmail.com>
2021-01-24 01:50:46 +0530

    added ad views and fixed bug in student views

--- backend/server/apps/tms/urls.py
+++ backend/server/apps/tms/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from apps.tms.views import (student_views)
+from apps.tms.views import (student_views,ad_views)
 
 urlpatterns = [
+    #student related URLs
     path('student/PS2TS/', student_views.PS2TS.as_view()),
     path('student/TS2PS/', student_views.TS2PS.as_view()),
     path('student/dashboard/',student_views.Dashboard.as_view()),
+    #AD related URLs
+    path('AD/dashboard/',ad_views.ADdashboard.as_view()),
 ]
